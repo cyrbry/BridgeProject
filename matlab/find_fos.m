8ac21c49b9e9c8a22348ecba7f6643c0323515b9
yang <mr.xywang@gmail.com>
2025-11-21 07:13:56 -0500

    spreadsheet

--- matlab/find_fos.m
+++ matlab/find_fos.m
@@ -46,6 +46,22 @@ fos_glue = GLUE_SHEAR / shear_glue
 p_euler = pi ^ 2 * E * INERTIA / (BRIDGE_LEN ^ 2);
 fos_euler_buckling = p_euler / MAX_LOAD
 
+function max_stress = thin_plate_buckling(K, t, b)
+  E = 4000;
+  MU = 0.2;
+  max_stress = K * pi^2 * E / (12 * (1 - MU^2)) * (t / b)^2
+end
+
+function fos_thin_plate = find_fos_thin_plate(K, t, b, stress_max)
+  fos_thin_plate = stress_max / thin_plate_buckling(K, t, b)
+end
+
+# case 1: compressive flange between webs
+
+fos_case1 = find_fos_thin_plate(4, 3*T, BETWEEN_WEB, MAT_COMP)
+
+
+
 b1 = 75 - 1.27 % top flange inside
 b2 = 12.5 + 1.27 % top flange flaps
 b3 = (112.7 - (4 * 1.27)) / 2 % height of web % VERY SUS CHECK THIS OUT
@@ -55,7 +71,7 @@ failure1 = 4 * (pi^2) * MAT_E * ((T * 3) ^ 2) / (12 * (1 - (MU ^ 2)) * (b1^2))
 FOSbuck1 = failure1 / (Mmax * (112.5 - CENTROID) / INERTIA)
 failure2 = 0.425 * (pi^2) * MAT_E * (T^2) / (12 * (1 - (MU^2)) * (b2^2))
 FOSbuck2 = failure2 / (Mmax * (112.5 - CENTROID) / INERTIA)
-failure3 = 6 * (pi^2) * MAT_E * ((2*T)^2) / (12 * (1 - (MU^2)) * (b3^2))
+failure3 = 6 * (pi^2) * MAT_E * (T^2) / (12 * (1 - (MU^2)) * (b3^2))
 FOSbuck3 = failure3 / (Mmax * (CENTROID - (1.27/2)) / INERTIA)
 failure4 = (5 * (pi^2) * MAT_E) ./ (12 * (1 - (MU^2))) .* (((2*T)/b4)^2 + ((2*T)./a).^2)
 FOSbuck4 = failure4 / shear
